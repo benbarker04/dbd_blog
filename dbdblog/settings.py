c92eefc34d9cd68119d048e13644ada764e3d1ad
Ben Barker <benbarker074@gmail.com>
2024-07-19 23:30:56 +0000

    add and run tests

--- dbdblog/settings.py
+++ dbdblog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 import dj_database_url
 
 if os.path.isfile('env.py'):
@@ -120,6 +121,9 @@ WSGI_APPLICATION = 'dbdblog.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 
 # Password validation
